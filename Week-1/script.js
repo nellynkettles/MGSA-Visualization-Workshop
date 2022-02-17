f5c1eb61823e84d1c525d4518c533a249fe4d193
Lia Petronio <lpetroni@broadinstitute.org>
2022-02-17 09:22:03 -0500

    basic file setup with data and variables

---
+++ Week-1/script.js
@@ -0,0 +1,54 @@
+
+/**
+ * 
+ * step 1: run a local host in your terminal
+ * python -m SimpleHTTPServer
+ * open browser with url http://localhost:8000/
+ */
+
+const width = d3.select('.plot').node().clientWidth,
+    height = d3.select('.plot').node().clientHeight;
+
+
+const data = [
+    {
+        x:5,
+        y:20,
+        category: "Dogs",
+        radius: 10
+    },
+    {
+        x:2,
+        y:7,
+        category: "Cats",
+        radius: 4
+    },
+    {
+        x:16,
+        y:6,
+        category: "Bunnies",
+        radius: 20
+    },
+    {
+        x:16,
+        y:17,
+        category: "Lizards",
+        radius: 12
+    }
+]
+console.log(data)
+let xScale;
+let yScale;
+let rScale;
+let colorScale;
+
+let plot = d3.select(".plot");
+
+let circles = plot.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", 100)
+    .attr("cy", 100)
+    .attr("r", 12)
+    .attr("fill", "red")
